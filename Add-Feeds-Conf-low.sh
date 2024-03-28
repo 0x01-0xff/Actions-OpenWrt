72e777349f8cf9385172c81d78dbd5c9558b6d2d
0x00 <18429669+0x01-0xff@users.noreply.github.com>
2024-03-28 18:05:06 +0800

    Update Add-Feeds-Conf-low.sh

--- Add-Feeds-Conf-low.sh
+++ Add-Feeds-Conf-low.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #
 # By Huson
-# 2024-03-27 03:21
+# 2024-03-28 18:04
 #
 # Add Default Feeds Src low version
+#
+# passwall 1&2 without sing-box
 #
 
 if [ ! "`grep "passwall" feeds.conf.default`" ]; then
 cat >> feeds.conf.default <<EOF
 src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
+#src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
+#src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;4.69-4
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;1.19-6
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git openclash https://github.com/vernesong/OpenClash.git
