ad706a0ceda976bcf13af0ca705281c8ea87095f
0x00 <18429669+0x01-0xff@users.noreply.github.com>
2024-04-07 16:32:56 +0800

    Update Add-Feeds-Conf.sh

--- Add-Feeds-Conf.sh
+++ Add-Feeds-Conf.sh
@@ -9,8 +9,8 @@
 if [ ! "`grep "passwall" feeds.conf.default`" ]; then
 cat >> feeds.conf.default <<EOF
 src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
-src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;4.73-3
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;1.25-5
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git openclash https://github.com/vernesong/OpenClash.git
 src-git subconverter https://github.com/tindy2013/openwrt-subconverter.git
