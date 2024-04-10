0a4a991befc6e8809ee9065078fa1a1569225b16
0x00 <18429669+0x01-0xff@users.noreply.github.com>
2024-04-10 19:18:07 +0800

    Update Add-Feeds-Conf-Older.sh

--- Add-Feeds-Conf-Older.sh
+++ Add-Feeds-Conf-Older.sh
@@ -10,8 +10,10 @@
 
 if [ ! "`grep "passwall" feeds.conf.default`" ]; then
 cat >> feeds.conf.default <<EOF
-src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;4.69-4
+#src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+# 4.69-4的tag号里只有passwall-packages，所以当pkg用
+src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall.git;4.69-4
+src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;1.19-6
 src-git helloworld https://github.com/fw876/helloworld.git
 #src-git openclash https://github.com/vernesong/OpenClash.git
