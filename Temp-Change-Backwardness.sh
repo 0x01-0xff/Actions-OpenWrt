371eaf57a74dba12d95840c0196d8f59ea8429e4
0x00 <18429669+0x01-0xff@users.noreply.github.com>
2024-04-15 12:57:21 +0800

    Update Temp-Change-Backwardness.sh

--- Temp-Change-Backwardness.sh
+++ Temp-Change-Backwardness.sh
@@ -19,7 +19,7 @@ CURL_PARAMS="curl -SsL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 
 #
 #######################################
 
-# **** get olded passwall packages ****
+# **** get older passwall packages ****
 echo ">> Set older passwall packages"
 rm -rf feeds/${PW_PKG_FEEDS_NAME}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/${PW_PKG_FEEDS_NAME}
@@ -27,7 +27,7 @@ cd feeds/${PW_PKG_FEEDS_NAME}
 git reset --hard 1946200
 cd ../..
 
-# **** get olded helloworld ****
+# **** get older helloworld ****
 echo ">> Set older helloworld"
 rm -rf feeds/${SSR_FEEDS_NAME}
 git clone https://github.com/fw876/helloworld.git feeds/${SSR_FEEDS_NAME}
@@ -39,4 +39,13 @@ cd ../..
 echo ">> Replace golang up to date"
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+
+# **** Sing-box GEODATA ****
+SB_GEO_DIR="files/usr/share/singbox"
+mkdir -p $SB_GEO_DIR
+#$CURL_PARAMS https://github.com/SagerNet/sing-geosite/releases/latest/download/geosite.db -o ${SB_GEO_DIR}/geosite.db 2>&1
+#$CURL_PARAMS https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db -o ${SB_GEO_DIR}/geoip.db 2>&1
+$CURL_PARAMS https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.db -o ${SB_GEO_DIR}/geosite.db 2>&1
+$CURL_PARAMS https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.db -o ${SB_GEO_DIR}/geoip.db 2>&1
+sudo chmod 644 ${SB_GEO_DIR}/geosite.db ${SB_GEO_DIR}/geoip.db
 
